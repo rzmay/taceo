2207666b422b2d37b3683198db8a6ecff53fbafa
Robert May <robertmay@Roberts-iMac.local>
2020-03-30 00:18:06 -0400

    Tutorial setup

---
+++ Assets/Scripts/Behaviours/TutorialSection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSection : MonoBehaviour
+{
+    
+    public List<TutorialSubsection> subsections = new List<TutorialSubsection>();
+
+    public float padding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        StartCoroutine(PlaySubsection(0));
+    }
+
+    private IEnumerator PlaySubsection(int index)
+    {
+        if (index < subsections.Count)
+        {
+            subsections[index].Play();
+            yield return new WaitForSeconds(subsections[index].audio.length + padding);
+            StartCoroutine(PlaySubsection(index + 1));
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviours/TutorialSubsection.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+using Util;
+
+public class TutorialSubsection : MonoBehaviour
+{
+    
+    private static readonly int TextIn = Animator.StringToHash("In");
+    private static readonly int TextOut = Animator.StringToHash("Out");
+
+    [System.Serializable]
+    public class TutorialText
+    {
+        public TMP_Text text;
+        public float entryTime;
+
+        [CanBeNull] 
+        public VibrationData vibration;
+
+        public Animator animator => _animator;
+        private Animator _animator;
+
+        public void SetAnimator()
+        {
+            _animator = text.GetComponent<Animator>();
+        }
+
+        public void Present()
+        {
+            _animator.SetTrigger(TextIn);
+            
+            vibration?.Start();
+        }
+
+        public void Hide()
+        {
+            _animator.SetTrigger(TextOut);
+        }
+
+        public IEnumerator Init(float audioDuration, float delay)
+        {
+            yield return new WaitForSeconds(entryTime);
+            Present();
+            yield return new WaitForSeconds(audioDuration - entryTime + delay);
+            Hide();
+        }
+    }
+
+    [SerializeField] 
+    public List<TutorialText> texts = new List<TutorialText>();
+    
+    public AudioClip audio;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (TutorialText tutorialText in texts)
+        {
+            tutorialText.SetAnimator();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pass
+    }
+
+    public void Play()
+    {
+        AudioSource.PlayClipAtPoint(audio, Vector3.zero);
+
+        foreach (TutorialText tutorialText in texts)
+        {
+            tutorialText.Init(audio.length, delay);
+        }
+    }
+}
